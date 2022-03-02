8d7d44c40f1a53212e8f838c0995a92d8ecd3dd3
unknown <chadiegilaugis@gmail.com>
2022-03-02 15:25:21 +0800

    prelim-dc

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <LottoComponent/>
+</template>
+
+<script>
+import LottoComponent from './components/LottoComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    LottoComponent    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
